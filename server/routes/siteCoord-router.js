92ae5078dca9fdfaa593d4c068557d63287fcfa7
Daniel <daniel.delrio@obf.ateneo.edu>
2020-07-02 22:55:37 +0800

    Add authentication to some routes

--- server/routes/siteCoord-router.js
+++ server/routes/siteCoord-router.js
@@ -7,8 +7,8 @@ const router = express.Router()
 router.post('/coords', passport.authenticate('jwt', { session: false }), SiteCoordCtrl.createCoords)
 router.put('/coords/:id', passport.authenticate('jwt', { session: false }), SiteCoordCtrl.updateCoords)
 router.delete('/coords/:id', passport.authenticate('jwt', { session: false }), SiteCoordCtrl.deleteCoords)
-router.get('/coords/:id', passport.authenticate('jwt', { session: false }), SiteCoordCtrl.getSiteCoordsById)
-router.get('/coords', passport.authenticate('jwt', { session: false }), SiteCoordCtrl.getAllSiteCoords)
-router.get('/coords/site/:code', passport.authenticate('jwt', { session: false }), SiteCoordCtrl.getSiteCoordsByCode)
+router.get('/coords/:id', SiteCoordCtrl.getSiteCoordsById)
+router.get('/coords', SiteCoordCtrl.getAllSiteCoords)
+router.get('/coords/site/:code', SiteCoordCtrl.getSiteCoordsByCode)
 
 module.exports = router;
--- server/routes/siteInfo-router.js
+++ server/routes/siteInfo-router.js
@@ -1,12 +1,12 @@
 const express = require('express')
-
 const SiteInfoCtrl = require('../controllers/siteInfo-ctrl')
+const passport = require('passport');
 
 const router = express.Router()
 
-router.post('/sitedata', SiteInfoCtrl.createData)
-router.put('/sitedata/:id', SiteInfoCtrl.updateData)
-router.delete('/sitedata/:id', SiteInfoCtrl.deleteData)
+router.post('/sitedata', passport.authenticate('jwt', { session: false }), SiteInfoCtrl.createData)
+router.put('/sitedata/:id', passport.authenticate('jwt', { session: false }), SiteInfoCtrl.updateData)
+router.delete('/sitedata/:id', passport.authenticate('jwt', { session: false }), SiteInfoCtrl.deleteData)
 router.get('/sitedata/:id', SiteInfoCtrl.getSiteDataById)
 router.get('/sitedata', SiteInfoCtrl.getAllSiteData)
 router.get('/sitedata/year/:year', SiteInfoCtrl.getSiteDataByYear)
