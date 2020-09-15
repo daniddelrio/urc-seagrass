1cbf0ca5aa9a4cce0ccf29eb550232a76cd95702
Daniel <daniel.delrio@obf.ateneo.edu>
2020-09-15 23:56:53 +0800

    Install Helmet and Compression

--- server/index.js
+++ server/index.js
@@ -6,24 +6,23 @@ const passport = require('passport');
 // const session = require("express-session");
 // const MongoStore = require("connect-mongo")(session);
 const MongoStore = require("connect-mongo");
+const compression = require('compression');
 
-const { dbUrl, port, jwtSecret } = require('./config');
+const { dbUrl, port, jwtSecret, corsOrigin } = require('./config');
 
 var app = express();
-// app.options('*', cors())
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-const corsOptions ={origin:'*'}
+const corsOptions ={
+	origin: corsOrigin
+}
 app.use(cors(corsOptions));
+
+// Compress HTTP responses
+app.use(compression());
 
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 const db = require('./db')
 
 require('./passport')(passport);
-// app.use(cors({origin: '*'}));
 app.use(passport.initialize());
 
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -32,6 +31,9 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 })); 
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded({extended: true})); // to support URL-encoded bodies
+
+// Protect app from well-known web vulnerabilities
+app.use(helmet());
 
 var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
@@ -57,10 +59,3 @@ app.use('/api', dataFieldsRouter)
 app.use('/api', datasetRouter)
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
-
-// Generic error handler used by all endpoints.
-function handleError(res, reason, message, code) {
-    console.log("ERROR: " + reason);
-    res.status(code || 500).json({"error": message});
-  }
-
